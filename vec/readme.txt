09485db80aa868e2d5238ed07f3fd19abbf2418b
Wayne <aamich@yahoo.com>
2019-08-29 00:57:41 -0700

    cuda programming vector addition

---
+++ vec/readme.txt
@@ -0,0 +1,11 @@
+Sample: vectorAdd
+Minimum spec: SM 3.0
+
+This CUDA Runtime API sample is a very basic sample that implements element by element vector addition. It is the same as the sample illustrating Chapter 3 of the programming guide with some additions like error checking.
+
+Key concepts:
+CUDA Runtime API
+Vector Addition
+
+compile:
+nvcc --std c++11 vectorAdd.cu -o va
---
+++ vec/vectorAdd.cu
@@ -0,0 +1,152 @@
+/**
+ * cuda programming example
+ */
+
+/**
+ * Vector addition: C = A + B.
+ *
+ * This sample is a very basic sample that implements element by element
+ * vector addition. It is the same as the sample illustrating Chapter 2
+ * of the programming guide with some additions like error checking.
+ */
+
+#include <iostream>
+#include <stdio.h>
+#include <time.h>
+
+using namespace std;
+
+__global__ void
+vectorAdd(const float *A, const float *B, float *C, int numElements)
+{
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+
+    if (i < numElements)
+    {
+        C[i] = A[i] + B[i];
+    }
+}
+
+__global__ void warmup()
+{
+    int i=1;
+    int j=2;
+    i=i+j;
+}
+
+/**
+ * Host main routine
+ */
+int
+main(void)
+{
+    // Error code to check return values for CUDA calls
+    cudaError_t err = cudaSuccess;
+
+    // Print the vector length to be used, and compute its size
+    int numElements = 100000000;
+    size_t size = numElements * sizeof(float);
+    printf("[Vector addition of %d elements]\n", numElements);
+
+    // Allocate the host input vector A
+    float *h_A = (float *)malloc(size);
+    float *h_B = (float *)malloc(size);
+    float *h_C = (float *)malloc(size);
+
+    // Verify that allocations succeeded
+    if (h_A == nullptr || h_B == nullptr || h_C == nullptr)
+    {
+        fprintf(stderr, "Failed to allocate host vectors!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialize the host input vectors
+    for (int i = 0; i < numElements; ++i)
+    {
+        h_A[i] = (float)i/(float)numElements;
+        h_B[i] = (float)i/(float)numElements;
+    }
+
+    // host computation
+    auto clk = clock();
+    for(int i = 0; i < numElements; ++i)
+        h_C[i] = h_A[i] + h_B[i];
+
+    cout << "CPU time: " << (clock()-clk)/ (float)CLOCKS_PER_SEC << " seconds" << endl;
+
+    //GPU warm-up
+    for(int i = 0; i < 4; ++i) warmup<<<1, 256>>>();
+
+    clk = clock();
+    // Allocate the device input vector A
+    float *d_A = nullptr;
+    float *d_B = nullptr;
+    float *d_C = nullptr;
+    cudaMalloc((void **)&d_A, size);
+    cudaMalloc((void **)&d_B, size);
+    cudaMalloc((void **)&d_C, size);
+
+    if(d_A == nullptr || d_B == nullptr || d_C == nullptr )
+    {
+        fprintf(stderr, "Failed to allocate device vector\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Copy the host input vectors A and B in host memory to the device input vectors in
+    // device memory
+    printf("Copy input data from the host memory to the CUDA device\n");
+    err = cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to copy vector A from host to device (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
+
+    // Launch the Vector Add CUDA Kernel
+    int threadsPerBlock = 256;
+    int blocksPerGrid =(numElements + threadsPerBlock - 1) / threadsPerBlock;
+    printf("CUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
+    vectorAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, numElements);
+    err = cudaGetLastError();
+
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to launch vectorAdd kernel (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    // Copy the device result vector in device memory to the host result vector
+    // in host memory.
+    printf("Copy output data from the CUDA device to the host memory\n");
+    cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
+    cout << "GPU time: " << (clock()-clk)/ (float)CLOCKS_PER_SEC << " seconds" << endl;
+
+    // Verify that the result vector is correct
+    for (int i = 0; i < numElements; ++i)
+    {
+        if (fabs(h_A[i] + h_B[i] - h_C[i]) > 1e-5)
+        {
+            fprintf(stderr, "Result verification failed at element %d!\n", i);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    printf("Test PASSED\n");
+
+    // Free device global memory
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
+    // Free host memory
+    free(h_A);
+    free(h_B);
+    free(h_C);
+
+    printf("Done\n");
+    return 0;
+}
+
